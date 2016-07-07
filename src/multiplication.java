70b013cb3681e2ceb5dadb49a01058f89ad35212
Alex Jensen <alex@Johns-MacBook-Pro.local>
2016-07-07 15:00:50 -0500

    remove redundant subclasses

--- src/multiplication.java
+++ src/multiplication.java
@@ -1,8 +1,14 @@
-abstract class Money {
-    protected int amount;
+class Money {
+    private int amount;
+    private String currency;
 
-    abstract Money times(int multiplier);
-    protected String currency;
+    Money times(int multiplier){
+        return new Money(amount * multiplier, currency);
+    }
+/*
+    Money plus(Money addend){
+        return new Money(amount + addend.amount, currency);
+    }*/
 
     Money(int amount, String currency){
         this.amount = amount;
@@ -13,43 +19,21 @@ abstract class Money {
         return currency;
     }
 
+    public String toString(){
+        return amount + " " + currency;
+    }
+
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency.equals(money.currency());
     }
 
     static Money dollar(int amount){
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount){
-        return new Franc(amount, "CHF");
-    }
-}
-
-class Dollar extends Money{
-
-    private String currency;
-
-    Dollar(int amount, String currency){
-        super(amount, currency);
-    }
-
-    Money times(int multiplier){
-        return Money.dollar(amount * multiplier);
-    }
-}
-
-class Franc extends Money{
-
-    private String currency;
-
-    Franc(int amount, String currency){
-        super(amount, currency);
-    }
-
-    Money times(int multiplier){
-        return Money.franc(amount * multiplier);
+        return new Money(amount, "CHF");
     }
 }
--- src/multiplicationTest.java
+++ src/multiplicationTest.java
@@ -13,10 +13,6 @@ public class multiplicationTest {
     public void testEquality(){
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -31,5 +27,11 @@ public class multiplicationTest {
     public void testCurrency(){
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
+    }
+
+    @org.junit.Test
+    public void testSimpleAddition(){
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
     }
 }
